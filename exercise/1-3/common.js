29e4174b2b3e7e90a55653ad1d94079068cc2e44
Tim Tsai <sky22357168@gmail.com>
2019-09-01 00:46:33 +0800

    1-3 1-4 Final

--- exercise/1-3/common.js
+++ exercise/1-3/common.js
@@ -3,7 +3,7 @@
     return response.text();
 }).then(data => {
 
-    // 1.請將data轉換成dom物件
-    // 2.將轉換好的dom物件只選取header的部分並且塞進header
-    // 3.將轉換好的dom物件只選取footer的部分並且塞進footer
+    // 1.請將data轉換成 dom element
+    // 2.將轉換好的 dom element 只選取header的部分並且塞進header
+    // 3.將轉換好的 dom element 只選取footer的部分並且塞進footer
 });
--- exercise/1-4/common.js
+++ exercise/1-4/common.js
@@ -2,7 +2,7 @@
     return response.text();
 }).then(data => {
 
-    // 1. 得到目前瀏覽器的url，並且取該url 最後一個 / 後的字
+    // 1. 得到目前的 url，並且取該 url 最後一個 / 後的字
     // 2. 將 Menu.html 塞進 menu 物件中
     // 3. 將 menu 中 其連結是目前url將它加上 selected的 class
 });
