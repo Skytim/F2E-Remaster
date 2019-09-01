0b60871a5bdee2074edc9075b6032a3d7e919465
Tim Tsai <sky22357168@gmail.com>
2019-09-02 00:06:48 +0800

    1-1 Finish

---
+++ complete/1-1-1/common.js
@@ -0,0 +1,10 @@
+﻿document.getElementById("header").onclick = clickHeader;
+document.getElementById("footer").onclick = clickFooter;
+
+var clickHeader = function () {
+    alert("我是header的部分");
+}
+
+var clickFooter = function () {
+    alert("我是footer的部分");
+}
---
+++ complete/1-1-2/common.js
@@ -0,0 +1,9 @@
+﻿var sidebar = document.getElementsByClassName("topic");
+
+alert("總共選到幾個元件" + sidebar.length);
+for (var i = 0; i < sidebar.length; i++) {
+    sidebar[i].onclick = function () {
+        // alert("作者是" + this.getAttribute("author"));
+        console.log("作者是" + this.getAttribute("author"));
+    }
+}
