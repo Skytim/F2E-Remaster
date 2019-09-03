27aeac9970e25ca7401f75821bec1d0a78813e20
Tim Tsai <sky22357168@gmail.com>
2019-09-03 22:42:06 +0800

    1-1-1

--- exercise/1-1-1/common.js
+++ exercise/1-1-1/common.js
@@ -1,8 +1,11 @@
-﻿var header = document.querySelector("#header");
+﻿var clickHeader = function () {
+    alert("我是header的部分");
+}
 
-header.addEventListener("click",function(){
-    console.log("我是header的部分");
-});
+
+document.getElementById("header").onclick = clickHeader;
+
+
 
 // 請完成
 // 1. 點選footer時，觸發alert事件
