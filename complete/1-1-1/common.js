c36ff1ae42ed184db8e466d753349de3deb0d6bb
Tim Tsai <sky22357168@gmail.com>
2019-09-03 22:40:24 +0800

    1-1 finish

--- complete/1-1-1/common.js
+++ complete/1-1-1/common.js
@@ -1,10 +1,11 @@
-﻿document.getElementById("header").onclick = clickHeader;
-document.getElementById("footer").onclick = clickFooter;
-
-var clickHeader = function () {
+﻿var clickHeader = function () {
     alert("我是header的部分");
 }
 
 var clickFooter = function () {
     alert("我是footer的部分");
 }
+
+document.getElementById("header").onclick = clickHeader;
+document.getElementById("footer").onclick = clickFooter;
+
