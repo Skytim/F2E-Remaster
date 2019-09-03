706e42d571e7750dd9ba78a17836e300f63495e4
Tim Tsai <sky22357168@gmail.com>
2019-09-03 22:52:18 +0800

    1-1-2

--- complete/1-1-2/common.js
+++ complete/1-1-2/common.js
@@ -1,8 +1,8 @@
-﻿var sidebar = document.getElementsByClassName("topic");
+﻿var topic = document.getElementsByClassName("topic");
 
-alert("總共選到幾個元件" + sidebar.length);
-for (var i = 0; i < sidebar.length; i++) {
-    sidebar[i].onclick = function () {
+alert("總共選到幾個元件" + topic.length);
+for (var i = 0; i < topic.length; i++) {
+    topic[i].onclick = function () {
         // alert("作者是" + this.getAttribute("author"));
         console.log("作者是" + this.getAttribute("author"));
     }
--- exercise/1-1-2/common.js
+++ exercise/1-1-2/common.js
@@ -1,10 +1,4 @@
-﻿var header = document.querySelector("#header");
-
-header.addEventListener("click",function(){
-    console.log("我是header的部分");
-});
-
-// 請完成
-// 1. 點選footer時，觸發alert事件
-// 2. 點選sidbar 的每一個 topic， 顯示其 author 內容 
+﻿// 請完成
+// 1. 透過 document.getElementsByClassName 取得目前的 topic 
+// 2. 點選 每一個  回應內容， 顯示其 author  
 // 3. 畫面上若有其他的 topic class，可以只限定 sidebar 內點選才觸發事件嗎?
