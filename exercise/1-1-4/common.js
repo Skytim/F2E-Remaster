fd21ff200d25b4ab97bf1c1557ec577c25d7e06d
Tim Tsai <sky22357168@gmail.com>
2019-09-07 20:39:18 +0800

    1-1-4 commit

---
+++ exercise/1-1-4/common.js
@@ -0,0 +1,21 @@
+﻿var header = document.querySelector("#header");
+var footer = document.querySelector("#footer");
+var sidebar = document.querySelectorAll("#sidebar .topic");
+
+
+header.onclick = function () {
+    console.log("我是header的部分");
+}
+
+footer.onclick = function () {
+    console.log("我是footer的部分");
+};
+
+console.log("總共選到幾個DOM元件:" + sidebar.length);
+
+for (var i = 0; i < sidebar.length; i++) {
+    sidebar[i].onclick = function () {
+        console.log("作者是"+this.getAttribute("author"));
+    }
+}
+// 請將上述的 onclick 方法改成  addEventListener 方法
