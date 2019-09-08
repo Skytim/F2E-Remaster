3d99a3b96e1f120fa1d3be113efeb466c824f327
Tim Tsai <sky22357168@gmail.com>
2019-09-08 15:15:59 +0800

    1-4 Review

--- complete/1-4/common.js
+++ complete/1-4/common.js
@@ -2,19 +2,21 @@
 var footer = document.querySelector("#footer");
 var sidebar = document.querySelectorAll("#sidebar .topic");
 
+var action = "mouseover";
 
-header.addEventListener("click",function(){
+header.addEventListener(action,function(){
     console.log("我是header的部分");
 })
 
-footer.addEventListener("click",function(){
+footer.addEventListener(action,function(){
     console.log("我是footer的部分");
 })
+
 
 console.log("總共選到幾個DOM元件:" + sidebar.length);
 
 for (var i = 0; i < sidebar.length; i++) {
-    sidebar[i].addEventListener("click",function(){
+    sidebar[i].addEventListener(action,function(){
         console.log("作者是"+this.getAttribute("author"));
     })
 }
