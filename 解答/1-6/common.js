5de05a670547bf51b0a4f12fb75e47fc226e97ec
Tim Tsai <sky22357168@gmail.com>
2019-09-10 23:59:23 +0800

    1-6 Rewview

--- 解答/1-6/common.js
+++ 解答/1-6/common.js
@@ -1,8 +1,9 @@
-﻿fetch('Menu.html').then(response => {
+﻿var menu = document.querySelector(".menu");
+
+fetch('Menu.html').then(response => {
     return response.text();
 }).then(data => {
     let url = location.href;
-    let href =url.substr(url.lastIndexOf("/") + 1);
     document.querySelector(".menu").innerHTML = data;
     document.querySelector("a[href='" + (href || "index.html") + "']").className = "selected";
 });
