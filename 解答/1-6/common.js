4fb2d5ee3b8ba49f03ba4525c5e25f949f4960fb
Tim Tsai <sky22357168@gmail.com>
2019-09-11 01:02:22 +0800

    1-6 Review

--- 解答/1-6/common.js
+++ 解答/1-6/common.js
@@ -4,6 +4,7 @@ fetch('Menu.html').then(response => {
     return response.text();
 }).then(data => {
     let url = location.href;
-    document.querySelector(".menu").innerHTML = data;
+    let href =url.substr(url.lastIndexOf("/") + 1);
+    menu.innerHTML = data;
     document.querySelector("a[href='" + (href || "index.html") + "']").className = "selected";
 });
