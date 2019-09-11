ee633d4a0c2f113fd00ae921ccd27555f06eef55
Tim Tsai <sky22357168@gmail.com>
2019-09-11 09:31:47 +0800

    1-6~ 1-7 Review

--- 解答/1-6/common.js
+++ 解答/1-6/common.js
@@ -3,8 +3,10 @@
 fetch('Menu.html').then(response => {
     return response.text();
 }).then(data => {
+    menu.innerHTML = data;
+    // 取得 Url
     let url = location.href;
     let href =url.substr(url.lastIndexOf("/") + 1);
-    menu.innerHTML = data;
+    // 選取 Url
     document.querySelector("a[href='" + (href || "index.html") + "']").className = "selected";
 });
